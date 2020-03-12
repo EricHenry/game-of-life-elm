17cda9b6b47b013ef1898e186e5d5900d80853be
Eric Correia <correia.eh@gmail.com>
2020-03-12 14:09:06 -0400

    Updates

---
+++ precache-manifest.ba3c2aadc0dd642c602d9c7b915be78e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/game-of-life-elm/favicon.ico"
+  },
+  {
+    "revision": "b28da0f14ecff64ee102321e24177939",
+    "url": "/game-of-life-elm/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/game-of-life-elm/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/game-of-life-elm/manifest.json"
+  },
+  {
+    "revision": "3136d6a12dfa60ce73a2",
+    "url": "/game-of-life-elm/static/css/main.3ba621fc.chunk.css"
+  },
+  {
+    "revision": "3136d6a12dfa60ce73a2",
+    "url": "/game-of-life-elm/static/js/main.3136d6a1.chunk.js"
+  },
+  {
+    "revision": "344063d8584c48bfbbf7",
+    "url": "/game-of-life-elm/static/js/runtime~main.344063d8.js"
+  },
+  {
+    "revision": "604c6712e7dcf2851e2c",
+    "url": "/game-of-life-elm/static/js/vendors~main.604c6712.chunk.js"
+  }
+]);
